d1b7bdbbe1becc31dcdf7d806fb7d272af92c584
juseong Yu <juseongyu56@gmail.com>
2025-08-05 17:50:32 +0900

    d

--- 실습/LIST/countinglist.py
+++ 실습/LIST/countinglist.py
@@ -4,6 +4,8 @@ def counting_sort(arr):
     result = [0] * len(arr)
     for ele in arr:
         counts[ele] += 1
+    for idx in range(len(counts) - 1):
+        counts[idx + 1] += counts[idx]
     for idx in range(len(arr) - 1, -1, -1):
         counts[idx] -= 1
         result[counts[arr[idx]]] = arr[idx]
