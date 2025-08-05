b443ecf4b79535e4651978a2930cc609c261325a
juseong Yu <juseongyu56@gmail.com>
2025-08-05 11:40:16 +0900

    카운팅 정렬

---
+++ 실습/LIST/countinglist.py
@@ -0,0 +1,9 @@
+def counting_sort(arr):
+    k = max(arr)
+    counts = [0] * (k + 1)
+    result = [0] * len(arr)
+    for ele in arr:
+        counts[ele] += 1
+    for idx in range(len(arr) - 1, -1, -1):
+        counts[idx] -= 1
+        result[counts[arr[idx]]] = arr[idx]
